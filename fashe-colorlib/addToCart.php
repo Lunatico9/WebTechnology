b8bd041c52ef8065ca81f13b409c587d1d9ea0ac
Federico Palmitesta <federico.palmitesta@gmail.com>
2019-02-18 00:12:32 +0100

    Update addToCart.php

--- fashe-colorlib/addToCart.php
+++ fashe-colorlib/addToCart.php
@@ -17,17 +17,37 @@ if(!isset($_SESSION['username'])){
     $_SESSION['userrole'] = 'g';
 }
 
-$quantity = $_POST['jqueryaiutamitu1'];
-$color = $_POST['jqueryaiutamitu2'];
-$size = $_POST['jqueryaiutamitu3'];
-$product = $_POST['jqueryaiutamitu4'];
+$quantity = $_POST['quantity'];
+$color = $_POST['color'];
+$size = $_POST['size'];
+$product = $_POST['product'];
 
 $query = "SELECT id FROM prodotto WHERE nome = '$product';"
 $result = queryMysql($query);
 $row = $result->fetch_row();
 $pid = $row[0];
 
+$quantity += checkProduct($color, $size);
+
 $user = $_SESSION['userid'];
 
 queryMysql("INSERT INTO carrello (cliente, prodotto, quantita, colore, taglia) VALUES ('$user', '$pid', '$quantity', '$color', '$size');");
 
+
+
+function checkProduct($color, $size) {
+    $query = "SELECT quantita, colore taglia FROM carrello WHERE cliente = '$userid' AND prodotto = $pid;"
+    $result = queryMysql($query);
+
+    if($result->num_rows > 0) {
+        for ($j = 0; $j < $result->num_rows; ++$j) {
+            $result->data_seek($j);
+            $product = $result->fetch_row();
+            if($color = $product[$j][1] && $size = $product[$j][2]) {
+                return $product[$j][0];
+            }
+        }
+    }
+
+    return 0;
+}
