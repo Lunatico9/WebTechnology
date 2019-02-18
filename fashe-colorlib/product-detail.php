1e8ee7c27d8beaabd1d80fc6e61db9268a3d67a2
Federico Palmitesta <federico.palmitesta@gmail.com>
2019-02-18 11:49:29 +0100

    Update product-detail.php

--- fashe-colorlib/product-detail.php
+++ fashe-colorlib/product-detail.php
@@ -1,6 +1,5 @@
 <?php
 
-
 require_once 'libs/Smarty.class.php';
 require_once 'function.php';
 require_once 'header.php';
@@ -126,32 +125,3 @@ else {
 }
 
 $smarty->display('product-detail.html');
-
-
-//image resize function
-function resize_image($file, $w, $h, $crop=FALSE) {
-    list($width, $height) = getimagesize($file);
-    $r = $width / $height;
-    if ($crop) {
-        if ($width > $height) {
-            $width = ceil($width-($width*abs($r-$w/$h)));
-        } else {
-            $height = ceil($height-($height*abs($r-$w/$h)));
-        }
-        $newwidth = $w;
-        $newheight = $h;
-    } else {
-        if ($w/$h > $r) {
-            $newwidth = $h*$r;
-            $newheight = $h;
-        } else {
-            $newheight = $w/$r;
-            $newwidth = $w;
-        }
-    }
-    $src = imagecreatefromjpeg($file);
-    $dst = imagecreatetruecolor($newwidth, $newheight);
-    imagecopyresampled($dst, $src, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
-
-    return $dst;
-}
